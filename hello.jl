31e058d721667c25bb4a9f619a4e483776929faf
Ryan Orsinger <ryan.orsinger@gmail.com>
2019-10-19 23:34:21 -0500

    first commit

---
+++ hello.jl
@@ -0,0 +1,11 @@
+using LinearAlgebra, SparseArrays, VMLS
+
+x = [1.1, 3.0, -9.22, 5.5]
+
+y = [2; 3; 4.3]
+
+println(x[1])
+
+z = ["first", "second", "third", "penultimate", "ultimate"]
+
+println(z[1])
